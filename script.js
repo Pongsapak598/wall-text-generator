75a66d23e0ec9f4c5e69a72d2789eb72baf48be1
pongsapak598 <pongsapak.roc@gmail.com>
2023-03-29 01:54:42 +0700

    Add text to image works

---
+++ script.js
@@ -0,0 +1,30 @@
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+const image = new Image();
+image.src = "image.png";
+
+image.onload = () => {
+  // Set the canvas width and height to match the image size
+  canvas.width = image.width;
+  canvas.height = image.height;
+
+  // Draw the image on the canvas
+  context.drawImage(image, 0, 0);
+};
+
+const overlay = document.querySelector(".overlay");
+const container = document.querySelector(".img-container");
+container.style.position = "relative";
+overlay.style.position = "absolute";
+overlay.style.top = "50%";
+overlay.style.left = "65%";
+overlay.style.transform = "translate(-50%, -50%)";
+
+const input = document.getElementById("text-input");
+input.addEventListener("input", () => {
+  if (input.value !== "") {
+    input.classList.add("border");
+  } else {
+    input.classList.remove("border");
+  }
+});
